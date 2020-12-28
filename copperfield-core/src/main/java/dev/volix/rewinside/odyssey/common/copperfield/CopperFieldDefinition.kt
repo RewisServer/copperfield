3f70065d12b0b8c5c92954728c4779cb3cf5db67
Benedikt Wüller <wuelleronline@gmail.com>
2020-12-28 13:36:02 +0100

    Add documentation to copper field definition

--- copperfield-core/src/main/java/dev/volix/rewinside/odyssey/common/copperfield/CopperFieldDefinition.kt
+++ copperfield-core/src/main/java/dev/volix/rewinside/odyssey/common/copperfield/CopperFieldDefinition.kt
@@ -3,4 +3,7 @@ package dev.volix.rewinside.odyssey.common.copperfield
 import dev.volix.rewinside.odyssey.common.copperfield.converter.Converter
 import java.lang.reflect.Field
 
+/**
+ * @author Benedikt Wüller
+ */
 data class CopperFieldDefinition<T : Any>(val field: Field, val name: String, val converter: Converter<T, Any>)
